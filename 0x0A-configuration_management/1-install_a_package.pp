84735b45ae41f9780b2748095813a7a6850b3aef
Zablon Samba <zablonsamba@gmail.com>
2024-01-19 14:59:49 +0300

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # A puppet manifest installing flask v2.1.0 which is a package from pip3.
-package { 'flask':
-  ensure   => 'installed',
-  provider => pip3,
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip'
 }
