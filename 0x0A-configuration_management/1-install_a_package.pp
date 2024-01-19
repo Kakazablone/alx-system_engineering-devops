7cb175b790a81da3bf94e48b1bf841fd4240b71e
Zablon Samba <zablonsamba@gmail.com>
2024-01-19 19:40:05 +0300

    Update config for flask installation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,10 @@
 # A puppet manifest installing flask v 2.1.0
-package {'python3':
-  ensure => 'installed'
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
 
-package {'python3-pip':
-  ensure  => 'installed',
-  require => Package['python3']
-}
-
-exec {'flask_installation':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep Version | grep 2.1.0',
-  require => Package['python3-pip']
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
