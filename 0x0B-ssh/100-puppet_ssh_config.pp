bd6df79c1cd7d0b6c3a38e916c0dece14b05d6d5
Zablon Samba <zablonsamba@gmail.com>
2024-01-20 18:55:00 +0300

    Create puppet manifest for .ssh config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,10 @@
 file { '/home/samba/.ssh/config':
   ensure  => present,
-  owner   => 'samba',
-  group   => 'samba',
-  mode    => '0600',
-  content => "# SSH client configuration\n
-              Host 100.25.102.191
-                  User ubuntu
-                  SendEnv LANG LC_*
-                  HashKnownHosts yes
-                  GSSAPIAuthentication yes
-                  GSSAPIDelegateCredentials no
-                  IdentityFile ~/.ssh/school
-                  PasswordAuthentication no\n"
+  content => [
+    '# Turn off passwd auth',
+    'PasswordAuthentication no',
+    '',
+    '# Declare identity file',
+    'IdentityFile ~/.ssh/school',
+  ].join("\n"),
 }
