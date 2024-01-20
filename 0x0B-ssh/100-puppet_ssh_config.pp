bc0d6a04743389fefa00b79ed35fd7933d812986
Zablon Samba <zablonsamba@gmail.com>
2024-01-20 18:25:44 +0300

    Create Puppet manifest that configures ssh for sever

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+file { '~/.ssh/config':
+  ensure  => present,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => "# SSH client configuration\n
+              Host 100.25.102.191
+                  User ubuntu
+                  IdentityFile ~/.ssh/school
+                  PasswordAuthentication no\n"
+}
