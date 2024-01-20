b869d9f4a8d7a502a3ed3017392114b8b71154c7
Zablon Samba <zablonsamba@gmail.com>
2024-01-20 20:49:36 +0300

    Create manifest for .ssh config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
-file { '/home/samba/.ssh/config':
-  ensure  => file,
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
   content => [
     '# Turn off passwd auth',
     'PasswordAuthentication no',
