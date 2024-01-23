c0f0479778c577c1e8a11987eeeaf262fba10a05
Zablon Samba <zablonsamba@gmail.com>
2024-01-23 21:45:48 +0300

    Create manifest for .ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 #Puppet manifest that alters config file
-file {'/etc/ssh/ssh_config':
+file { '/etc/ssh/ssh_config':
   ensure  => present,
   content =>"
   #config .ssh for server connect
   Host*
   IdentityFile ~/.ssh/school
-  PasswordAuthentication no
-  "
+  PasswordAuthentication no"
 }
