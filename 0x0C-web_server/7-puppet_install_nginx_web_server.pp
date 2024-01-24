8f83c047dc37262899a749b5b4015ff4fe3706a0
Zablon Samba <zablonsamba@gmail.com>
2024-01-24 09:10:20 +0300

    Create a Puppet Manifest that installs Nginx and redirects user

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -13,9 +13,6 @@ service { 'nginx':
   require => Package['nginx'],
 }
 
-exec { 'configure_nginx_redirect':
-  command  => 'sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/github.com\/Kakazablone\/;\\n\\t}/" /etc/nginx/sites-available/default',
-  provider => shell,
-  require  => Service['nginx'],
-  notify   => Service['nginx'],
+exec { 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/github.com\/Kakazablone\/;\\n\\t}/" /etc/nginx/sites-available/default',
+  provider => shell
 }
