85fbb5e8a5d27aab7741fc10c93c62d3be0c922e
Zablon Samba <zablonsamba@gmail.com>
2024-01-29 14:36:09 +0300

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,39 +12,6 @@ service { 'nginx':
   require => Package['nginx'],
 }
 
-# Index.html page
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Hello World!',
-  require => Service['nginx'],
-}
-
-# 301 Redirection
-file_line { 'redirect_me_location':
-  path   => '/etc/nginx/sites-available/default',
-  line   => '        location /redirect_me {',
-  match  => '^.*server_name _;.*$',
-  before => '        location / {',
-  notify => Service['nginx'],
-}
-
-file_line { 'redirect_me_return':
-  path   => '/etc/nginx/sites-available/default',
-  line   => '            return 301 https://github.com/Kakazablone;',
-  match  => '^.*location /redirect_me {.*$',
-  after  => '        location /redirect_me {',
-  notify => Service['nginx'],
-}
-
-# Custom 404 page
-file_line { 'custom_404_page':
-  path   => '/etc/nginx/sites-available/default',
-  line   => '        error_page 404 /404.html;',
-  match  => '^.*server_name _;.*$',
-  before => '        location / {',
-  notify => Service['nginx'],
-}
-
 # Nginx header
 file_line { 'custom_nginx_header':
   path   => '/etc/nginx/sites-available/default',
