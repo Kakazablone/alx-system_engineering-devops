7e8c3529e66585ca5944e3073f448d09b3781e30
Zablon Samba <zablonsamba@gmail.com>
2024-02-19 19:19:18 +0300

    Activate API Foundations

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Accesses a REST API for todo lists of employees"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    emp_Id = argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = f"{baseUrl}/{emp_Id}"
+
+    with requests.get(url) as response:
+        emp_Name = response.json().get('name')
+
+    todoUrl = f"{url}/todos"
+
+    with requests.get(todoUrl) as response:
+        tasks = response.json()
+
+    done_tasks = [task for task in tasks if task.get('completed')]
+    done = len(done_tasks)
+
+    print(f"Employee {emp_Name} is done with tasks ({done}/{len(tasks)}):")
+
+    for task in done_tasks:
+        print(f"\t{task.get('title')}")
