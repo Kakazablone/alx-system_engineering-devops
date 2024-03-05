dff1a8f0fa2e4505c53f9e7fc241e73c2a7faf7f
Zablon Samba <zablonsamba@gmail.com>
2024-03-05 16:54:58 +0300

    function that queries the Reddit API and prints the titles of the first     10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,17 +11,14 @@ def top_ten(subreddit):
     function that queries the Reddit API and prints the titles of the first
     10 hot posts listed for a given subreddit
     """
-
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
-
     user_agent = {'User-agent': 'VICTORY'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
     response = get(url, headers=user_agent, params=params)
     results = response.json()
-
     try:
         rqd_info = results.get('data').get('children')
 
