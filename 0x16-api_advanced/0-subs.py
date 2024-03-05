ec1be1a9d9c5f584ef41bcee4677814e4979c85d
Zablon Samba <zablonsamba@gmail.com>
2024-03-05 16:42:01 +0300

    Queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Module that tests out the Reddit API '''
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    '''
+    Queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit
+    '''
+    headers = {'User-Agent': 'VICTORY'}
+    response = get(
+        'https://www.reddit.com/r/{}/about.json'.format(str(subreddit)),
+        headers=headers,
+        allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json()
+    return data.get('data').get('subscribers')
