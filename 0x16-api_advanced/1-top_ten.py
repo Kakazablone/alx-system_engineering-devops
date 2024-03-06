36f1bbe9acaa9b002db5e52a58812f447ea0c431
Zablon Samba <zablonsamba@gmail.com>
2024-03-06 09:56:19 +0300

    queries the Reddit API and prints the titles of the first     10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-'''Module that tests out the Reddit API'''
-import requests
+"""
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+from requests import get
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot
-    posts listed for a given subreddit
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'VICTORY'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
 
-    if response.status_code == 200:
-        data = response.json()
-        for post in data['data']['children']:
-            print(post['data']['title'])
-    else:
+    if subreddit is None or not isinstance(subreddit, str):
         print("None")
 
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-if __name__ == '__main__':
-    import sys
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
 
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
